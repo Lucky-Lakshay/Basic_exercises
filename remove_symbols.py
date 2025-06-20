9002ec26ad15ed4c616a2f8b81b743e66223e7a4
Lucky-Lakshay <luckatmyside99@gmail.com>
2025-06-20 16:30:34 +0530

    # Remove special symbols / punctuation from a string using re module

--- remove_symbols.py
+++ remove_symbols.py
@@ -1,15 +1,13 @@
 # Remove special symbols / punctuation from a string
 
 import re
-import string
 
 str1 = "/*Jon is @developer & musician"
 print("Original string is: ", str1)
 
-# Solution 1: Use string functions translate() and maketrans()
-new_str = str1.translate(str.maketrans("", "", string.punctuation))
-print("New string is: ", new_str)
+# Solution: Using regex replace pattern in a string
+# ^ negates, anything inside [] is what ur matching in string: w words, s spaces, d digits
+# if u remove ^ everything u r matching will be removed
 
-# Solution 2: Using regex replace pattern in a string
 res = re.sub(r"[^\w\s]", "", str1)
 print("New string is ", res)
